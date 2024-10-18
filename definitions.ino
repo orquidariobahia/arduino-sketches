6d68b764b2f09c4a593914e69a6d4ab29b86f923
Shirkit <Shirkit@users.noreply.github.com>
2024-10-18 13:22:32 -0300

    docs: :memo: Created a definitions file for future reference

---
+++ definitions.ino
@@ -0,0 +1,22 @@
+// Sensors ID (0-39)
+
+// #define TEMP_ID 1
+// #define HUM_ID 2
+// #define CO2_ID 3
+// #define TVOC_ID 4
+// #define AQI_ID 5
+
+// Actuators ID (60-99)
+
+// #define RELAY_ID 60
+
+// Configurations (120-159)
+
+// #define AUTONOMOUS_ID 120
+// #define UPDATE_FREQUENCY_ID 121
+// #define TEMPERATURE_ADJUST_ID 122
+// #define HUMIDITY_ADJUST_ID 123
+// #define MIN_HUMIDITY_ID 124
+// #define MAX_HUMIDITY_ID 125
+// #define MIN_CO2_ID 126
+// #define MAX_CO2_ID 127
